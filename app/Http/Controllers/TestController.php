3d0ecbdb827b5c00d5533a1d42d1ac1761f625c3
Sava Bezanovic <sava.bezanovic@quantox.com>
2020-04-23 16:02:37 +0200

    folder:Repositories, Services files: UserService, UserRepository,TestController, test view, UserRequest code: post and get routes for test view

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -17,26 +17,26 @@ class TestController extends Controller
 
     public function test()
     {
+
+        $response = "Ne radi smece!";
      
         // $response = Http::withHeaders([
         //     'x-rapidapi-host' => 'imdb-internet-movie-database-unofficial.p.rapidapi.com',
 	    //     'x-rapidapi-key' => 'a1de5906e7mshe0cf46dd3e674cap1ba97ajsncdbf9fe795c4'
         // ])
         // ->setRequestUrl('https://imdb-internet-movie-database-unofficial.p.rapidapi.com/film/tt1375666')
-        // ->get('/film/{query}', [
-        //     'id' => 'tt1375666'
+        // ->get();
+        
+        // $response = Http::withHeaders([
+        //     'x-rapidapi-host' => 'imdb-internet-movie-database-unofficial.p.rapidapi.com',
+	    //     'x-rapidapi-key' => 'a1de5906e7mshe0cf46dd3e674cap1ba97ajsncdbf9fe795c4'
+        // ])
+        // ->setRequestUrl('https://imdb-internet-movie-database-unofficial.p.rapidapi.com/film/tt1375666')
+        // ->post('http://localhost/test', [
+        //     'id' => 'tt1375666',
         // ]);
 
-        $response = Http::withHeaders([
-            'x-rapidapi-host' => 'imdb-internet-movie-database-unofficial.p.rapidapi.com',
-	        'x-rapidapi-key' => 'a1de5906e7mshe0cf46dd3e674cap1ba97ajsncdbf9fe795c4'
-        ])
-        ->setRequestUrl('https://imdb-internet-movie-database-unofficial.p.rapidapi.com/film/tt1375666')
-        ->post('http://localhost/movie-box/public/test', [
-            'id' => 'tt1375666',
-        ]);
-
-        // return view("test")->with("response", $response);
+        return view("test")->with("response", $response);
 
     }
     
