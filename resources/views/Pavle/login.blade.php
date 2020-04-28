ba7222752220cd5fc8739c5a90fa9a28911d6aae
pavle.birac <pavle.birac@quantox.com>
2020-04-28 15:59:41 +0200

    Login, SignUp

---
+++ resources/views/Pavle/login.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<head>
+<link rel="stylesheet" type="text/css" href="{{ asset('css/login-sign.css') }}" >
+</head>
+<body>
+    <div class="login-page">
+        <span class="welcome">Welcome to MovieBox</span>
+        <div class="form">
+        <form class="login-form">
+            <input type="text" placeholder="username"/>
+            <input type="password" placeholder="password"/>
+            <button>login</button>
+            <p class="message">Not registered? <a href="#">Create an account</a></p>
+            <p class="message"><a href="">Go back</a></p>
+        </form>
+        </div>
+    </div>
+</body>
---
+++ resources/views/Pavle/signup.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<head>
+<link rel="stylesheet" type="text/css" href="{{ asset('css/login-sign.css') }}" >
+</head>
+<body>
+    <div class="login-page">
+    <span class="welcome">Welcome to MovieBox</span>
+        <div class="form">
+        <form class="login-form">
+            <input type="text" placeholder="name"/>
+            <input type="password" placeholder="password"/>
+            <input type="text" placeholder="email address"/>
+            <button>create</button>
+            <p class="message">Already registered? <a href="#">Sign In</a></p>
+            <p class="message"><a href="">Go back</a></p>
+        </form>
+        </div>
+    </div>
+</body>
