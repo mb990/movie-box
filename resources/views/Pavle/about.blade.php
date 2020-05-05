f48c4ba75810527ee7497906936152c4f0f1aa7c
pavle.birac <pavle.birac@quantox.com>
2020-05-05 09:33:17 +0200

    About page

---
+++ resources/views/Pavle/about.blade.php
@@ -0,0 +1,17 @@
+@extends('pavle.master')
+@section('title','About')
+
+@section('header')
+<p class="center">This site is made as intern project.</p>
+
+@endsection
+
+@section('main')
+<div class="center">
+    <h5>For any additional information, contact us:</h5>
+        <h5>Backend - spasoje
+            <br>
+            Frontend - pavle.birac@quantox.com
+        </h5>
+</div>
+@endsection
