d85b919cde296be3a370fa9a25b53521099d3ab5
pavle.birac <pavle.birac@quantox.com>
2020-05-11 12:18:52 +0200

    Mobile view pages

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -5,7 +5,7 @@
 
             <div class="logIn"><a href="{{route('login')}}"><button class="btn-login button-recommended">LOG IN</button></div></a>
             <div class="signUp"><a href="{{route('register')}}"><button class="active-color button-recommended">SIGN UP</button></div></a>
-
+            
         @endguest
 
         @auth()
