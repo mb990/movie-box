97729ee59aed3958ebd3477990311349ac6a8d4d
pavle.birac <pavle.birac@quantox.com>
2020-05-12 10:12:10 +0200

    Comment replay, edit update

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,10 +3,13 @@
     <head>
         <title>@yield('title'){{$title}}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Find movie you looking for, like movie or watch online trailer for your favorite movie.">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,600">
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}" >
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     </head>
     <body>
         @include('includes.header')
