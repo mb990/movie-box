ccbaa800246deb346129f09a0c91a1e7f4c2e042
pavle.birac <pavle.birac@quantox.com>
2020-05-12 13:48:38 +0200

    Actor page

---
+++ resources/views/actor.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.master')
+@section('title', "")
+
+@section('main')
+<div class="movies-list">
+
+    <span class="movie-name center">Actor name</span>
+    <span>List of movies this actor is acting: </span>
+    <br>
+    <span>
+            <span class="commas font-new">movie names</span>
+    </span>
+    <a href="/" class="center">Click here for more info about Actor name</a>
+
+</div>
+@endsection
